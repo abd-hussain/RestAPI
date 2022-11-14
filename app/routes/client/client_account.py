3cf266c835d33a3e0a4e76864c5d6192eae3118d
Abdelrahman <abdelrahman@JOD0242L-2.local>
2022-11-14 22:08:08 +0300

    handle allow disallow notifications

--- app/routes/client/client_account.py
+++ app/routes/client/client_account.py
@@ -24,51 +24,6 @@ async def get_account(request: Request, db: Session = Depends(get_db), get_curre
        return generalResponse(message="profile was not found", data=None)
 
     return generalResponse(message="Profile return successfully", data=query.first())
-    
-# @router.put("/update")
-# async def update_account(payload: UpdateClientAccountModel , request: Request, db: Session = Depends(get_db), get_current_user: int = Depends(get_current_user)):
-#     myHeader = validateLanguageHeader(request)
-#     query = db.query(DB_Client_Users).filter(DB_Client_Users.id == get_current_user.user_id)
-    
-#     if query.first() == None:
-#        return generalResponse(message="profile was not found", data=None)
-
-#     if query.first().invitation_code is None:
-#         query.update({"invitation_code" : generateActvationCode()}, synchronize_session=False)
- 
-#     if payload.first_name != None:
-#         query.update({"first_name" : payload.first_name}, synchronize_session=False)
-#     if payload.last_name != None:
-#         query.update({"last_name" : payload.last_name}, synchronize_session=False)
-#     if payload.mobile_number != None:
-#         query.update({"mobile_number" : payload.mobile_number}, synchronize_session=False)
-#     if payload.email != None:
-#         query.update({"email" : payload.email}, synchronize_session=False)
-#     if payload.gender != None:
-#         query.update({"gender" : payload.gender}, synchronize_session=False)
-#     if payload.referal_code != None:
-#         query.update({"referal_code" : payload.referal_code}, synchronize_session=False)
-#     if payload.profile_img != None:
-#         query.update({"profile_img" : payload.profile_img}, synchronize_session=False)
-#     if payload.os_type != None:
-#         query.update({"os_type" : payload.os_type}, synchronize_session=False)
-#     if payload.device_type_name != None:
-#         query.update({"device_type_name" : payload.device_type_name}, synchronize_session=False)
-#     if payload.os_version != None:
-#         query.update({"os_version" : payload.os_version}, synchronize_session=False)
-#     if payload.app_version != None:
-#         query.update({"app_version" : payload.app_version}, synchronize_session=False)
-#     if payload.date_of_birth != None:
-#         query.update({"date_of_birth" : payload.date_of_birth}, synchronize_session=False)
-#     if payload.country_id != None:
-#         query.update({"country_id" : payload.country_id}, synchronize_session=False)
-#     if payload.allow_notifications != None:
-#         query.update({"allow_notifications" : payload.allow_notifications}, synchronize_session=False)
-        
-#     db.commit()
-#     return generalResponse(message="Profile updated successfully", data=query.first())
-
-
 
 @router.put("/update")
 async def update_account(request: Request,first_name: str = Form(None),last_name: str = Form(None),email: str = Form(None),gender: int = Form(None),
