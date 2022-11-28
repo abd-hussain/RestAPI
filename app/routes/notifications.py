c2beedb848cf3688bf949bc87a02714388fb7369
Abdelrahman <abdelrahman@JOD0242L-3.local>
2022-11-28 13:05:31 +0300

    .

--- app/routes/notifications.py
+++ app/routes/notifications.py
@@ -19,11 +19,11 @@ async def get_Posts(request: Request, db: Session = Depends(get_db), get_current
     
     data = db.query(DB_Notifications.id, DB_Notifications.title_english.label(
         "title"), DB_Notifications.content_english.label(
-        "content"), DB_Notifications.readed).filter(DB_Notifications.client_owner_id == get_current_user.user_id).filter(DB_Notifications.title_english.contains(search)).limit(limit).offset(skip).all()
+        "content"), DB_Notifications.readed, DB_Notifications.created_at).filter(DB_Notifications.client_owner_id == get_current_user.user_id).filter(DB_Notifications.title_english.contains(search)).limit(limit).offset(skip).all()
     if (myHeader.language == "ar"):
         data = db.query(DB_Notifications.id, DB_Notifications.title_arabic.label(
             "title"), DB_Notifications.content_arabic.label(
-            "content"), DB_Notifications.readed).filter(DB_Notifications.client_owner_id == get_current_user.user_id).filter(DB_Notifications.title_arabic.contains(search)).limit(limit).offset(skip).all()
+            "content"), DB_Notifications.readed, DB_Notifications.created_at).filter(DB_Notifications.client_owner_id == get_current_user.user_id).filter(DB_Notifications.title_arabic.contains(search)).limit(limit).offset(skip).all()
     return generalResponse(message="list of notifications return successfully", data=data)
 
 
