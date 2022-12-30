80e957edaba05902f931ea7b29e4c6ee7739aa10
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2022-12-30 23:41:21 +0300

    get appointments

--- app/routes/appointment.py
+++ app/routes/appointment.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.models.schemas.appointment import AppointmentRequest
 from app.utils.database import get_db
 from app.models.database.db_appointment import DB_Mentors_Reservations
+from app.models.database.mentor.db_mentor_user import DB_Mentor_Users
 from app.utils.oauth2 import get_current_user
 from app.utils.validation import validateLanguageHeader
 from datetime import datetime
@@ -18,12 +19,18 @@ async def get_all_mentor_appointment(id :int , request: Request, db: Session = D
     myHeader = validateLanguageHeader(request)
     query = db.query(DB_Mentors_Reservations).filter(DB_Mentors_Reservations.mentor_id == id).filter(DB_Mentors_Reservations.date_from > datetime.now()).all()
     return generalResponse(message="list of appointments return successfully", data=query)
-
-
+    
 @router.get("/client")
 async def get_clientAppointment(request: Request, db: Session = Depends(get_db), get_current_user: int = Depends(get_current_user)):
     myHeader = validateLanguageHeader(request)
-    query = db.query(DB_Mentors_Reservations).filter(DB_Mentors_Reservations.date_from > datetime.now()).all()
+    query = db.query(DB_Mentors_Reservations.id, DB_Mentors_Reservations.date_from, DB_Mentors_Reservations.date_to, 
+                     DB_Mentors_Reservations.client_id, DB_Mentors_Reservations.mentor_id, 
+                     DB_Mentors_Reservations.price_before_discount, DB_Mentors_Reservations.discount_id, DB_Mentor_Users.suffixe_name, 
+                     DB_Mentor_Users.first_name, DB_Mentor_Users.last_name,
+                     ).join(DB_Mentor_Users, DB_Mentor_Users.id == DB_Mentors_Reservations.mentor_id, isouter=True).filter(DB_Mentors_Reservations.client_id == get_current_user.user_id).all()
+    
+    
+    
     return generalResponse(message="list of appointments return successfully", data=query)
 
 @router.post("/book")
