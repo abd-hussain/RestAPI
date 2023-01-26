d4cf2c6e00b7fb1d91f988a3817adfa3ff6f3ea3
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-01-26 11:05:08 +0300

    handle send email

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -1,28 +1,51 @@
 import os
-from email.message import EmailMessage
-import ssl
-import smtplib
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from fastapi import FastAPI, BackgroundTasks
 
-email_sender = os.environ.get("email_sender")
-email_password = os.environ.get("email_password")
-body = """
-Test Email """
+from dotenv import load_dotenv
+load_dotenv('.env')
 
 
-def send_email_async(subject: str, email_to: str):
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_to
-    em['Subject'] = "Test"
-    em.set_content(body)
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_to, em.as_string())
+class Envs:
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_FROM = os.getenv('MAIL_FROM')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
 
 
+conf = ConnectionConfig(
+    MAIL_USERNAME=Envs.MAIL_USERNAME,
+    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
+    MAIL_FROM=Envs.MAIL_FROM,
+    MAIL_PORT=Envs.MAIL_PORT,
+    MAIL_SERVER=Envs.MAIL_SERVER,
+    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
+    USE_CREDENTIALS=True,
+    MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=False,
+    TEMPLATE_FOLDER='./app/utils/templates/email'
+)
 
-
-
+# async def send_email_async(subject: str, email_to: str, body: dict):
+#     message = MessageSchema(
+#         subject=subject,
+#         recipients=[email_to],
+#         body=body,
+#         subtype='html',
+#     )
+    
+#     fm = FastMail(conf)
+#     await fm.send_message(message, template_name='email.html')
+    
+def send_email_background(background_tasks: BackgroundTasks, subject: str, email_to: str):
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        body="s",
+        subtype='html',
+    )
+    fm = FastMail(conf)
+    background_tasks.add_task(
+       fm.send_message, message, template_name='email.html')
