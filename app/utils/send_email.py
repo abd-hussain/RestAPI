840645a55306f4d44033ddfc8dc456c8f804551f
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-01-26 11:22:39 +0300

    handle send email

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -10,7 +10,6 @@ class Envs:
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_FROM = os.getenv('MAIL_FROM')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
 
@@ -27,17 +26,6 @@ conf = ConnectionConfig(
     MAIL_STARTTLS=False,
     TEMPLATE_FOLDER='./app/utils/templates/email'
 )
-
-# async def send_email_async(subject: str, email_to: str, body: dict):
-#     message = MessageSchema(
-#         subject=subject,
-#         recipients=[email_to],
-#         body=body,
-#         subtype='html',
-#     )
-    
-#     fm = FastMail(conf)
-#     await fm.send_message(message, template_name='email.html')
     
 def send_email_background(background_tasks: BackgroundTasks, subject: str, email_to: str):
     message = MessageSchema(
