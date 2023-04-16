306de5d7fb0c571d62059b95c77cb2124dcf2d72
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-04-16 23:35:38 +0300

    changing image

--- app/routes/client/client_account.py
+++ app/routes/client/client_account.py
@@ -7,6 +7,8 @@ from app.utils.oauth2 import get_current_user
 from app.utils.validation import validateLanguageHeader
 from app.utils.generate import generateActvationCode, generateRequestId
 from app.models.schemas.client.client_account import UpdateClientAccountModel
+import shutil
+import os
 
 router = APIRouter(
     prefix="/client-account",
@@ -38,10 +40,11 @@ async def delete_account(request: Request, db: Session = Depends(get_db), get_cu
 
 @router.put("/update")
 async def update_account(request: Request,first_name: str = Form(None),last_name: str = Form(None),email: str = Form(None),gender: int = Form(None),
-                         country_id: int = Form(None), referal_code: str = Form(None),date_of_birth: str = Form(None),profile_picture: UploadFile = File(default=None), 
+                         country_id: int = Form(None), referal_code: str = Form(None),date_of_birth: str = Form(None),profile_picture: UploadFile = File(None), 
                          os_type: str = Form(""),device_type_name: str = Form(""),app_version: str = Form(""),
                          db: Session = Depends(get_db), get_current_user: int = Depends(get_current_user)):
     myHeader = validateLanguageHeader(request)
+    query_account = db.query(DB_Client_Users).filter(DB_Client_Users.id == get_current_user.user_id).first()
     query = db.query(DB_Client_Users).filter(DB_Client_Users.id == get_current_user.user_id)
     payload = UpdateClientAccountModel(first_name = first_name, last_name = last_name, email = email, date_of_birth = date_of_birth, 
                                      country_id = country_id, gender = gender, referal_code = referal_code, 
@@ -52,14 +55,19 @@ async def update_account(request: Request,first_name: str = Form(None),last_name
 
     if payload.first_name != None:
         query.update({"first_name" : payload.first_name}, synchronize_session=False)
+        
     if payload.last_name != None:
         query.update({"last_name" : payload.last_name}, synchronize_session=False)
+        
     if payload.mobile_number != None:
         query.update({"mobile_number" : payload.mobile_number}, synchronize_session=False)
+        
     if payload.email != None:
         query.update({"email" : payload.email}, synchronize_session=False)
+        
     if payload.gender != None:
         query.update({"gender" : payload.gender}, synchronize_session=False)
+        
     if payload.referal_code != None:
         query.update({"referal_code" : payload.referal_code}, synchronize_session=False)
     if payload.profile_img != None:
@@ -79,27 +87,28 @@ async def update_account(request: Request,first_name: str = Form(None),last_name
         
     if profile_picture is not None:
         content_type = profile_picture.content_type
-        profile_picture.file.seek(0, 2)
         file_size = profile_picture.file.tell()
         if file_size > 2 * 1024 * 1024:
         # more than 2 MB
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={"message": f"File too large", "request_id": generateRequestId()})
-        # if content_type not in ["image/jpeg", "image/png", "image/jpg"]:
-        #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={"message": f"Profile Image Format is not valid", "request_id": generateRequestId()})
+        if content_type not in ["image/jpeg", "image/png", "image/jpg", "image/gif"]:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={"message": f"Profile Image Format is not valid", "request_id": generateRequestId()})
 
-        file_location = f"static/profileImg/{get_current_user.user_id}.png"
-        
+        upload_dir = os.path.join(os.getcwd(), "static/clientsImg")
         try:
-            contents = profile_picture.file.read()
-            with open(file_location, 'wb+') as out_file:
-                out_file.write(contents)
-                query.update({"profile_img" : file_location}, synchronize_session=False)
-        except Exception:
-            return {"message": "There was an error uploading the file"}
-        finally:
-            profile_picture.file.close()
+            os.remove(f"static/clientsImg/{query_account.profile_img}")
+        except OSError:
+            pass
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+                        
+        dest = os.path.join(upload_dir, f"{get_current_user.user_id}.{content_type[6:]}")
         
-    db.commit()
+        with open(dest, "wb") as buffer:
+            
+            shutil.copyfileobj(profile_picture.file, buffer)
+            query.update({"profile_img" : f"{get_current_user.user_id}.{content_type[6:]}"}, synchronize_session=False)
+            db.commit()
     
     return generalResponse(message="Profile updated successfully", data=query.first())
 
