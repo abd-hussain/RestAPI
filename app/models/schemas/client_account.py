4d29adab3182f0e2c248ab3b6dba3e3dafa1ae57
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-01-20 15:57:56 +0300

    handle auth

--- app/models/schemas/client_account.py
+++ app/models/schemas/client_account.py
@@ -4,9 +4,6 @@ from pydantic import BaseModel
 class ClientAccountModel(BaseModel):
     mobile_number : str
     country_id: int
-    os_type: str
-    device_type_name: str
-    os_version: str
     app_version: str
     last_otp: Optional[str]
     api_key: Optional[str]
