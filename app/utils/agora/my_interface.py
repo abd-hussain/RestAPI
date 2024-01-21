a64b3e3b3324091f8044236513cb4f811ee6bec6
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-01-21 18:54:05 +0300

    handle time

--- app/utils/agora/my_interface.py
+++ app/utils/agora/my_interface.py
@@ -5,7 +5,7 @@ from app.utils.agora.src.RtcTokenBuilder import *
 appID = "67fa993d64a346e1a2587f4a8b96f569"
 appCertificate = "534094b2fbda4bb7b93fbdcfb55daf79"
 uid = 0
-expireTimeInSeconds = 3600
+expireTimeInSeconds = 14400
 currentTimestamp = int(time.time())
 privilegeExpiredTs = currentTimestamp + expireTimeInSeconds
 
