064116d8448380ed34853e62ba3714f34cb836a1
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-01-21 14:46:53 +0300

    prepare api for active appointents

--- app/routes/client/client_appointment.py
+++ app/routes/client/client_appointment.py
@@ -72,6 +72,48 @@ async def get_client_appointments(request: Request, db: Session = Depends(get_db
     
     return generalResponse(message="list of appointments return successfully", data=query)
 
+
+@router.get("/active")
+async def get_client_active_appointments(request: Request, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+    myHeader = validateLanguageHeader(request)
+    category_column = DB_Categories.name_arabic if myHeader.language == "ar" else DB_Categories.name_english
+    currency_column = DB_Appointments.currency_arabic if myHeader.language == "ar" else DB_Appointments.currency_english
+    country_column = DB_Countries.name_arabic if myHeader.language == "ar" else DB_Countries.name_english
+
+    query = db.query(DB_Appointments.id, 
+                     DB_Appointments.date_from, 
+                     DB_Appointments.date_to, 
+                     DB_Appointments.mentor_id, 
+                     DB_Appointments.appointment_type, 
+                     DB_Appointments.state,
+                     DB_Appointments.discount_id,
+                     DB_Appointments.is_free,
+                     DB_Appointments.price,
+                     DB_Appointments.discounted_price,
+                     currency_column.label("currency"), 
+                     DB_Appointments.mentor_hour_rate,
+                     DB_Appointments.note_from_client, 
+                     DB_Appointments.note_from_mentor, 
+                     DB_Appointments.channel_id,
+                     
+                     DB_Mentor_Users.profile_img, 
+                     DB_Mentor_Users.suffixe_name, 
+                     DB_Mentor_Users.first_name, 
+                     DB_Mentor_Users.last_name, 
+                     DB_Mentor_Users.speaking_language,
+                     country_column.label("countryName"), 
+                     category_column.label("categoryName"), 
+                     ).join(DB_Mentor_Users, DB_Mentor_Users.id == DB_Appointments.mentor_id, isouter=True
+                     ).join(DB_Categories, DB_Categories.id == DB_Mentor_Users.category_id, isouter=True
+                     ).join(DB_Countries, DB_Countries.id == DB_Mentor_Users.country_id, isouter=True
+                     ).filter(DB_Appointments.client_id == current_user.user_id,
+                                DB_Mentor_Users.blocked == False,
+                                DB_Mentor_Users.published == True,
+                                DB_Appointments.state == AppointmentsState.active
+                     ).all()
+    
+    return generalResponse(message="list of active appointments return successfully", data=query)
+
 @router.post("/cancel")
 async def cancel_appointment(id: int, request: Request, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     myHeader = validateLanguageHeader(request)
