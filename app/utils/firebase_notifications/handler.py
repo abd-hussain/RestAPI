43d6f1c298fc11b72887633be3d5949f2f576d01
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-04-02 16:16:29 +0300

    .

--- app/utils/firebase_notifications/handler.py
+++ app/utils/firebase_notifications/handler.py
@@ -1,9 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-# cred = credentials.Certificate("./app/utils/firebase_notifications/serviceAccountKey.json")
+cred = credentials.Certificate("./app/utils/firebase_notifications/serviceAccountKey.json")
 
-# firebase_admin.initialize_app(cred, name='legalzhub')
+firebase_admin.initialize_app(cred, name='legalzhub')
 
 def send_push_notification(token, title, body):
     app = firebase_admin.get_app('legalzhub')
